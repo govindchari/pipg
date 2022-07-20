302d636dbd7922dc39202dd4d0e0bf71175e7594
govindchari <govindchari1@gmail.com>
2022-07-19 22:53:38 -0700

    Added halfspace projection

--- include/projection.hpp
+++ include/projection.hpp
@@ -3,7 +3,10 @@
 using namespace Eigen;
 
 void project_box(VectorXd &x, const VectorXd l, const VectorXd u){
-    // assert(l < u);
+    assert(l.rows() == u.rows());
+    assert(x.rows() == l.rows());
+    assert(x.rows() == u.rows());
+    assert(l.cwiseMin(u) == l);
     x = x.cwiseMin(u).cwiseMax(l); 
 }
 
@@ -14,6 +17,10 @@ void project_ball(VectorXd &x, const double r){
     }
 }
 
-void project_halfspace(VectorXd x, VectorXd c, double a){
-    // static_assert(dim(x) == dim(c));
+void project_halfspace(VectorXd &x, VectorXd c, double a){
+    assert(x.rows() == c.rows());
+    assert(a != 0);
+    if (c.dot(x) > a){
+        x = x - (c.dot(x) - a) * (c / (c.norm()*c.norm()));
+    }
 }

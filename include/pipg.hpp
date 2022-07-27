6a89f8096b857587bca3f737bd47b4fef6956035
govindchari <govindchari1@gmail.com>
2022-07-26 19:36:54 -0700

    Updated pipg.hpp

--- include/pipg.hpp
+++ include/pipg.hpp
@@ -5,43 +5,58 @@
 #define EIGEN_RUNTIME_NO_MALLOC
 
 #include "projection.hpp"
+#include "utilities.hpp"
 
 using namespace Eigen;
-class MPC{
-    private:
-        // Time Horizon
-        int T;
+class MPC
+{
+private:
+    // Time Horizon
+    size_t T;
+    size_t nx;
+    size_t nu;
 
-        // Problem Data
-        std::vector<MatrixXd> A;
-        std::vector<MatrixXd> B;
-        std::vector<MatrixXd> C;
-        std::vector<MatrixXd> D;
+    // Problem Data
+    std::vector<MatrixXd> A;
+    std::vector<MatrixXd> B;
+    std::vector<MatrixXd> Q;
+    std::vector<MatrixXd> R;
 
-        // Optimization Variables
-        std::vector<MatrixXd> X;
-        std::vector<MatrixXd> U;
+    // Optimization Variables
+    std::vector<VectorXd> X;
+    std::vector<VectorXd> U;
 
-        // Proportional and Integral Terms
-        std::vector<MatrixXd> V;
-        std::vector<MatrixXd> W;
+    // Proportional and Integral Terms
+    std::vector<VectorXd> V;
+    std::vector<VectorXd> W;
 
-        // Eta Parameters
-        double eta1;
-        double eta2;
-        double eta3;
+    // Eta Parameters
+    double eta1;
+    double eta2;
+    double eta3;
+    bool eta1_outdated;
+    bool eta2_outdated;
+    bool eta3_outdated;
 
-    public:
-        void addA(const size_t t, const MatrixXd A);
-        void addB(const size_t t, const MatrixXd B);
-        void addQ(const size_t t, const MatrixXd Q);
-        void addR(const size_t t, const MatrixXd R);
-        void addBallConstraint(const size_t t, const enum variable, const double r);
-        void addBoxConstraint(const size_t t, const enum variable, const VectorXd l, const VectorXd u);
-        void addHalfspaceConstraint(const size_t t, const enum variable, const VectorXd c, const double a);
-        void solve();
-        void solve(bool verbose);
+    // Workspace
+    WORKSPACE ws;
 
-        MPC(const size_t T);
+    // void updateEta1();
+    void updateEta2();
+    void updateEta3();
 
+public:
+    void updateEta1();
+    void printQR();
+    void addA(const size_t t, const MatrixXd Ain);
+    void addB(const size_t t, const MatrixXd Bin);
+    void addQ(const size_t t, const MatrixXd Qin);
+    void addR(const size_t t, const MatrixXd Rin);
+    // void addBallConstraint(const size_t t, const enum variable, const double r);
+    // void addBoxConstraint(const size_t t, const enum variable, const VectorXd l, const VectorXd u);
+    // void addHalfspaceConstraint(const size_t t, const enum variable, const VectorXd c, const double a);
+    void solve();
+    void solve(bool verbose);
+
+    MPC(const size_t T_horizon, const size_t nxin, const size_t nuin);
 };
--- include/projection.hpp
+++ include/projection.hpp
@@ -2,28 +2,32 @@
 #include "Eigen/Dense"
 #define EIGEN_RUNTIME_NO_MALLOC
 
-
 using namespace Eigen;
 
-void project_box(VectorXd &x, const VectorXd l, const VectorXd u){
+inline void project_box(VectorXd &x, const VectorXd l, const VectorXd u)
+{
     assert(l.rows() == u.rows());
     assert(x.rows() == l.rows());
     assert(x.rows() == u.rows());
     assert(l.cwiseMin(u) == l);
-    x = x.cwiseMin(u).cwiseMax(l); 
+    x = x.cwiseMin(u).cwiseMax(l);
 }
 
-void project_ball(VectorXd &x, const double r){
+inline void project_ball(VectorXd &x, const double r)
+{
     assert(r > 0);
-    if (x.norm() > r){
-        x = (r/x.norm())*x;
+    if (x.norm() > r)
+    {
+        x = (r / x.norm()) * x;
     }
 }
 
-void project_halfspace(VectorXd &x, VectorXd c, double a){
+inline void project_halfspace(VectorXd &x, VectorXd c, double a)
+{
     assert(x.rows() == c.rows());
     assert(a != 0);
-    if (c.dot(x) > a){
-        x = x - (c.dot(x) - a) * (c / (c.norm()*c.norm()));
+    if (c.dot(x) > a)
+    {
+        x = x - (c.dot(x) - a) * (c / (c.norm() * c.norm()));
     }
 }
